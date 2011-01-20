938dcf0465dba2b22b20c87b757ea4681f6b903d
Mikael Hvidtfeldt Christensen <mikaelhc@gmail.com>
2011-01-20 23:03:22 +0100

    Fixed Linux Build Script

--- Fragmentarium-Source/Build - Linux/build.sh
+++ Fragmentarium-Source/Build - Linux/build.sh
@@ -7,12 +7,13 @@ make
 
 cd "Build - Linux"
 
+rm -rf Fragmentarium
 mkdir "Fragmentarium"
 mkdir "Fragmentarium/Examples"
 cp -r ../Examples/* "Fragmentarium/Examples"
 mkdir "Fragmentarium/Misc"
 cp -r ../Misc/* "Fragmentarium/Misc"
-cp ../../trunk "Fragmentarium/Fragmentarium"
+cp ../Fragmentarium-Source "Fragmentarium/Fragmentarium"
 
 cd "Fragmentarium"
 rm -rf `find . -type d -name .svn`
